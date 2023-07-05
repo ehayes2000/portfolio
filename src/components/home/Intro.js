08f6b46f1837dcafccece3e7857c897ef885d9c5
eric <eqhayes@protonmail.com>
2023-07-04 22:28:24 -0700

    rework colors

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -3,9 +3,9 @@ import Social from "./Social";
 const Intro = () => (
   <div className="flex items-center justify-center min-h-screen text-neutral-900">
   <div className="flex flex-col justify-center lg:mr-60 md:mr-4 space-y-4">
-    <h2 className="text-left transition-all duration-500">
+    {/* <h2 className="text-left transition-all duration-500">
       I Am
-    </h2>
+    </h2> */}
     <h1 className="text-left transition-all duration-500">
       Eric Hayes
     </h1>
