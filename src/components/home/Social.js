3371e9089924e946177d8d5cf4a6f778463c9736
eric <eqhayes@protonmail.com>
2023-07-13 18:42:36 -0700

    fix link

--- src/components/home/Social.js
+++ src/components/home/Social.js
@@ -3,7 +3,7 @@ import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Social = () => (
     <div className="flex transition ease-in duration-150"> 
-        <a className="mr-2 transition transform hover:scale-105 hover:-translate-y-1" href="www.linkedin.com/in/erichayes8" target="_blank" rel="noopener noreferrer">
+        <a className="mr-2 transition transform hover:scale-105 hover:-translate-y-1" href="https://www.linkedin.com/in/erichayes8/" target="_blank" rel="noopener noreferrer">
             <FaLinkedin size={35} className="text-stone-950"/>
         </a>
         <a className="transition transform hover:scale-105 hover:-translate-y-1" href="https://github.com/ehayes2000" target="_blank" rel="noopener noreferrer">
